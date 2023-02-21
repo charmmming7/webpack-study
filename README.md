83b613b50675cd0086cc3f6fcccf1624260a38ac
sujipark <sujipark@linecorp.com>
2023-02-21 10:22:03 +0900

    1. npm 설치 후, 엔트리와 아웃풋 세팅

---
+++ README.md
@@ -0,0 +1,10 @@
+# webpack-study
+
+프론트엔드 개발환경(webpack, babel, eslint..) 강의 내용 정리.
+
+
+- 강의 주소: [https://www.inflearn.com/course/프론트엔드-개발환경](https://www.inflearn.com/course/프론트엔드-개발환경)
+
+- 강의 노트: [김정환 블로그](https://jeonghwan-kim.github.io/posts/?series=%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%94%EB%93%9C%20%EA%B0%9C%EB%B0%9C%ED%99%98%EA%B2%BD%EC%9D%98%20%EC%9D%B4%ED%95%B4)
+
+- 실습(github) 주소: [jeonghwan-kim/lecture-frontend-dev-env](jeonghwan-kim/lecture-frontend-dev-env)
---
+++ src/math.js
@@ -0,0 +1,28 @@
+// 1. 전역 공간에 sum이 노출
+/*
+function sum(a, b) {
+  return a + b
+}
+*/
+
+// 2. IIFE 방식의 모듈
+/*
+var math = math || {}; // 전역 네임스페이스. math가 있으면 할당, 없으면 빈객체 할당.
+
+(function(){
+	function sum(a, b) {
+	  return a + b
+	}
+
+	math.sum = sum; // sum 함수를 외부에서 접근할 수 있도록 전역 네임스페이스인 math에 sum 함수 할당
+})();
+*/
+
+// 3-1. CommonJS 모듈방식
+// exports function sum(a, b) { return a + b; }
+
+
+// 3-2. ES2015에서 표준 모듈 시스템 
+export function sum(a, b) {
+  return a + b
+}
