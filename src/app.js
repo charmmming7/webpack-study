d63dd8f3ce810ec21b4d05b2bdadb1bd2b4b5843
sujipark <sujipark@linecorp.com>
2023-03-06 21:38:06 +0900

    2. 로더

--- src/app.js
+++ src/app.js
@@ -1,17 +1,30 @@
-// 1. 원시방식
+// ----------------------------------
+// 1. 인풋/아웃풋
+
+// 1) 원시방식
 // sum(1, 2);
 
-// 2. IIFE 방식의 모듈
+// 2) IIFE 방식의 모듈
 // math.sum(1, 2);
 
-// 3-1. CommonJS 모듈방식
+// 3-1) CommonJS 모듈방식
 /*
 const math = require("./math.js")
 math.sum(1, 2) // 3
 */
 
-// 3-2. ES2015에서 표준 모듈 시스템 
+// 3-2) ES2015에서 표준 모듈 시스템 
 // import * as math from "./math.js";
-import { sum } from './math.js';
+// import { sum } from './math.js';
+// console.log(sum(1, 2));
 
-console.log(sum(1, 2));
+// ----------------------------------
+// 2. 로더
+import './app.css';
+import nyancat from './nyancat.jpg';
+
+document.addEventListener('DOMContentLoaded', () => { 
+  document.body.innerHTML = `
+    <img src="${nyancat}">
+  `
+})
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path = require("path");
 module.exports = {
   mode: 'development',
   entry: {
-    main: './src/app.js'
+    main: './src/app.js' // 상대경로
   },
   output: {
-    path: path.resolve('./dist'),
-    filename: '[name].js'
+    path: path.resolve('./dist'), // 절대경로를 해석해주는 node 모듈
+    filename: '[name].js' // 동적파일명
   },
   // module: {
   //   rules: [
@@ -18,5 +18,33 @@ module.exports = {
   //       ]
   //     }
   //   ]
-  // }
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', // js -> cssom
+          'css-loader' // css -> js
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          publicPath: './dist/', // 파일명 앞에 추가되는 문자열(경로)
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      // {
+      //   test: /\.(png|jpg|gif|svg)$/,
+      //   loader: 'url-loader',
+      //   options: {
+      //     publicPath: './dist/', // 파일명 앞에 추가되는 문자열(경로)
+      //     name: '[name].[ext]?[hash]',
+      //     limit: 20000, // 20kb 미만이면 base64로 변환
+      //   }
+      // }
+    ]
+  }
 }
